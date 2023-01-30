948c2d9a766bf7aa254cd6d832807d6acd332c19
Te3Ka_PaynE <Mnement4813@yandex.ru>
2023-01-30 09:59:48 +0300

    Задание 1. Сделано

--- Exercise01/Program.cs
+++ Exercise01/Program.cs
@@ -21,10 +21,12 @@ void PrintArray (int[,] printMatrix)
     {
         for(int j = 0; j < printMatrix.GetLength(1); j++)
         {
-            if(printMatrix[i, j] == 0)
+            if (printMatrix[i, j] == 0)
                 Console.Write($"  ");
+            else if (printMatrix[i, j] == 1)
+                Console.Write($"X ");
             else
-                Console.Write($"{printMatrix[i, j]} ");
+                Console.Write($"0 ");
         }
         Console.WriteLine();
     }
@@ -57,4 +59,19 @@ int[,] matrix = new int[,]
     { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 };
 
+void FillImage(int row, int columns)
+{
+    if (matrix[row, columns] == 0)
+    {
+        PrintArray(matrix);
+        matrix[row, columns] = 2;
+        FillImage(row - 1, columns);
+        FillImage(row, columns - 1);
+        FillImage(row + 1, columns);
+        FillImage(row, columns + 1);
+    }
+}
+
+PrintArray(matrix);
+FillImage(13, 13);
 PrintArray(matrix);
